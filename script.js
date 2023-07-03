4920a3252379daf8c0346daf4f2edff19ae1ca36
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-07-03 16:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateTime() {
+let timer=document.getElementById("timer")
+let date = new Date();
+timer.innerHTML=date.toLocaleString();
+}
+updateTime();
+setInterval(updateTime, 1000);
